75c96add43fc7ad4d1afb89af2f7a3869d38bc5c
phuctv95 <phuctv95@gmail.com>
2019-02-19 14:26:02 +0700

    Update basic-linq-query-operations_6.cs

--- docs/csharp/programming-guide/concepts/linq/codesnippet/CSharp/basic-linq-query-operations_6.cs
+++ docs/csharp/programming-guide/concepts/linq/codesnippet/CSharp/basic-linq-query-operations_6.cs
@@ -1,14 +1,14 @@
               // queryCustomersByCity is an IEnumerable<IGrouping<string, Customer>>
-                var queryCustomersByCity =
-                    from cust in customers
-                    group cust by cust.City;
+              var queryCustomersByCity =
+                  from cust in customers
+                  group cust by cust.City;
 
-                // customerGroup is an IGrouping<string, Customer>
-                foreach (var customerGroup in queryCustomersByCity)
-                {
-                    Console.WriteLine(customerGroup.Key);
-                    foreach (Customer customer in customerGroup)
-                    {
-                        Console.WriteLine("    {0}", customer.Name);
-                    }
-                }
+              // customerGroup is an IGrouping<string, Customer>
+              foreach (var customerGroup in queryCustomersByCity)
+              {
+                  Console.WriteLine(customerGroup.Key);
+                  foreach (Customer customer in customerGroup)
+                  {
+                      Console.WriteLine("    {0}", customer.Name);
+                  }
+              }
